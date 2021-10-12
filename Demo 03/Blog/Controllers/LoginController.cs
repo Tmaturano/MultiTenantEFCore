e9778588a5dd96f501d29e3e8d02b268e5070a4a
Thiago Maturana <thiagomatu@gmail.com>
2021-10-12 10:44:41 -0300

    added the demo 3 (one database, multiple tenants)

---
+++ Demo 03/Blog/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Blog.Data;
+using Blog.Entities;
+using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    // One database
+    // the connection string can be stored in a "central database", but be aware to encrypt it well
+    // Or it can be stored in the Connection String section in Azure (or other cloud provider), so it will be automatically changed when deployed
+    [ApiController]
+    [Route("login")]
+    public class LoginController : ControllerBase
+    {
+        private readonly AppDataContext _context;
+
+        public LoginController(AppDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody]User model)
+        {            
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == model.Email);
+            if (user is null) return NotFound();
+
+            if (!PasswordHasher.Verify(user.PasswordHash, model.PasswordHash))
+                return BadRequest(new { Message = "User or password invalid" });
+                        
+            var token = TokenService.GenerateToken(user);
+            return new
+            {
+                user = user.Email,
+                token
+            };            
+        }
+    }
+}
---
+++ Demo 03/Blog/Data/Mapping/UserMap.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(x => x.Id);
+
+            builder
+                .HasMany(x => x.Roles)
+                .WithMany(x => x.Users)
+                .UsingEntity(x => x.ToTable("UserRole")); ;
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(80).HasColumnType("VARCHAR");
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(200).HasColumnType("VARCHAR");
+            builder.Property(x => x.PasswordHash).IsRequired().HasMaxLength(255).HasColumnType("VARCHAR");
+            builder.Property(x => x.Bio).IsRequired().HasColumnType("TEXT");
+            builder.Property(x => x.Image).IsRequired().HasMaxLength(200).HasColumnType("VARCHAR");
+            builder.Property(x => x.Slug).IsRequired().HasMaxLength(80).HasColumnType("VARCHAR");
+        }
+    }
+}
